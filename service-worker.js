f56d6206881b0e0ec361e4229aee630441eba353
vabhi2024 <vabhi2024@gmail.com>
2025-03-19 06:07:43 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "abhi-unitech-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME)
+                          .map(name => caches.delete(name))
+            );
+        })
+    );
+});
+
